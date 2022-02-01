12b0e917b3cd136d299378d4a48d1c91c2dbce7a
Jim Garrison <garrison@ibm.com>
2022-02-01 16:49:39 -0500

    Reformat test_template_class.py and setup.py using black

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as fp:
+with open("requirements.txt") as fp:
     install_requires = fp.read()
 
 setuptools.setup(
@@ -14,5 +14,5 @@ setuptools.setup(
     long_description=long_description,
     packages=setuptools.find_packages(),
     install_requires=install_requires,
-    python_requires='>=3.7'
+    python_requires=">=3.7",
 )
--- tests/test_template_class.py
+++ tests/test_template_class.py
@@ -6,6 +6,7 @@ from prototype_template.template_class import TemplateClass
 
 class TestPrototypeTemplate(TestCase):
     """Tests prototype template."""
+
     def test_template_class(self):
         """Tests template class."""
         obj = TemplateClass(1)
