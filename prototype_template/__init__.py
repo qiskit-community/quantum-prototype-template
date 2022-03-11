f92b23f839b8fc32267d69f40694d340fa3f7c7d
Jim Garrison <garrison@ibm.com>
2022-03-10 23:09:26 -0500

    Set `__version__` in the module from VERSION.txt

--- prototype_template/__init__.py
+++ prototype_template/__init__.py
@@ -1,3 +1,17 @@
 """Template project."""
 
 from .template_module import TemplateClass
+
+
+def __get_version():
+    import os
+    with open(
+        os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "VERSION.txt",
+        ),
+        "r",
+    ) as f:
+        return f.read().strip()
+
+__version__ = __get_version()
