11b3890d98421efbfdf85cc70d0f61c69b37e747
Jim Garrison <garrison@ibm.com>
2022-04-15 20:38:47 -0400

    Enable setuptools_scm via setup.py
    https://github.com/pypa/setuptools_scm#setuppy-usage-deprecated

--- setup.py
+++ setup.py
@@ -16,4 +16,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=install_requires,
     python_requires=">=3.7",
+    setup_requires=['setuptools_scm'],
+    use_scm_version=True,
 )
