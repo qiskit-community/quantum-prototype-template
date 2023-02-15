b2b6839c76d0be496c4e746ea66d6f21cb56bb88
caleb-johnson <caleb-johnson@users.noreply.github.com>
2023-02-15 20:01:25 +0000

    deploy: c2a6be7b69a4c70085012554420a77bca5f8511f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+gc321e77',
+    VERSION: '0.1.dev1+gc2a6be7',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
